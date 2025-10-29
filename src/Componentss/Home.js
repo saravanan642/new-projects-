ed59cb77ce47b9dbcfc29a88e21f20caaa557c44
saravanan642 <saravanansenthil605@gmail.com>
2025-10-29 11:48:19 +0530

    fisrt commit

---
+++ src/Componentss/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p className=' font-bold text-yellow-700'> OCEAN vibes</p>
+      <p className=' font-bold text-yellow-700'> OCEAN vibes</p>
+      <p className=' font-bold text-yellow-700'> OCEAN vibes</p>
+      <p className=' font-bold text-yellow-700'> OCEAN vibes</p>
+      <p className=' font-bold text-yellow-700'> OCEAN vibes</p>
+      <p className=' font-bold text-yellow-700'> OCEAN vibes</p>
+      <p className=' font-bold text-yellow-700'> OCEAN vibes</p>
+
+      <p>fieiifijssssssssssssssifnieie</p>
+    </div>
+  )
+}
+
+export default Home
